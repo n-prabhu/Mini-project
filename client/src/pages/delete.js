396d67805d620a97362be204e0291e95a0535d8b
n-prabhu <naveen.prabhu@byjus.com>
2021-12-15 20:44:09 +0530

    delete functionallity

--- client/src/pages/delete.js
+++ client/src/pages/delete.js
@@ -1,24 +1,50 @@
 import Header from "../components/header/header";
+import axios from "axios";
+import { useState } from "react";
+import  DeleteModal  from "../components/deleteModal";
+import AlertBox from "../components/alert";
 
 const Delete = () => {
-  const SearchAccount = () => {};
 
-  const DeleteAccount = () => {};
+  const [errMessage,setErrMessage ] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [showAlert,setShowAlert] = useState(false);
+  const [searchedValue,setSearchedValue]=useState();
+  
+  const deleteAccount = async() => {
+    const searchedPhoneNumber = await document.getElementById('searchBox').value;
+    setSearchedValue(searchedPhoneNumber);
+    await axios.get(`http://localhost:8000/api/users/findone/${searchedPhoneNumber}`)
+      .then((response)=>{
+        if(response.data.data === null){
+          setShowAlert(true);
+          setErrMessage("Account not found");
+        }else{
+          setShowAlert(false);
+          setOpen(true);
+        }
+      })
+    
+    
+  };
 
   return (
     <div>
       <Header />
-      <div className="m-5">
-        <input
-          className="form-control mt-4"
-          type="text"
-          placeholder="Find a Account for update"
-          onChange={SearchAccount}
-        ></input>
-        <button className="btn btn-dark" onClick={DeleteAccount}>
-          Delete
-        </button>
+      <div className="d-flex flex-row justify-content-center">
+        <div className="p-3 w-50">
+          <input type="Number"
+          className="form-control" 
+          placeholder="Phone Number"
+          aria-label="Phone Number"
+          id="searchBox"/>
+        </div>
+        <div className="p-3">
+          <button className="btn btn-dark" onClick={deleteAccount}>Delete</button>    
+        </div>
       </div>
+      {showAlert ? <div className="w-50 text-center m-auto"><AlertBox message={errMessage}/></div> : null}
+      <DeleteModal openModal={open} setOpenModel={setOpen} phoneNumber={searchedValue}/>
     </div>
   );
 };
